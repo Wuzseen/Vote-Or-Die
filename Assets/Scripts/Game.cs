14716262c7922789cac6993aecb159a1b5548db3
Timothy Day <the.tday@gmail.com>
2014-08-09 17:40:16 -0400

    Network hello world

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+	public delegate void GameProcess();
+	public static event GameProcess OnRoundEnd;
+	public int numOfPlayers;
+
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("1.0");
+	}
+
+	void OnReceivedRoomListUpdate() {
+		PhotonNetwork.JoinOrCreateRoom("TestRoom",new RoomOptions(),null);
+	}
+
+	void OnJoinedRoom() {
+		PhotonNetwork.Instantiate("Player",Vector3.zero,Quaternion.identity,0);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public Trait trait;
+	public Goal goal;
+	
+	// Use this for initialization
+	void Awake () {
+		Game.OnRoundEnd += GoalCompleted;
+		print ("Hello");
+	}
+
+	protected void GoalCompleted() {
+		print ("Hello");
+	}
+
+	void OnDestroy() {
+		Game.OnRoundEnd -= GoalCompleted;
+	}
+}
